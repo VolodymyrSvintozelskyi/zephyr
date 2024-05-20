1885cf54b2c9e351b3506f9612fcc224c99270cf
Volodymyr Svintozelskyi <volodymyrsvintozelskyi@gmail.com>
2024-05-20 22:10:29 +0300

    Added RTE handler

--- soc/riscv/riscv-privileged/neorv32/soc.c
+++ soc/riscv/riscv-privileged/neorv32/soc.c
@@ -6,6 +6,72 @@
 
 #include <zephyr/irq.h>
 #include <soc.h>
+#include <stdio.h>
+
+static void __attribute__((__naked__,aligned(4))) rte_handler(void) {
+	// intro
+	printf("<NEORV32-RTE> ");
+
+	// privilege level of the CPU when the trap occurred
+	if (csr_read(mstatus) & (3 << 11)) {
+		printf("[M] "); // machine-mode
+	}
+	else {
+		printf("[U] "); // user-mode
+	}
+
+	// cause
+	uint32_t trap_cause = csr_read(mcause);
+	switch (trap_cause) {
+		case TRAP_CODE_I_MISALIGNED: printf("Instruction address misaligned"); break;
+		case TRAP_CODE_I_ACCESS:     printf("Instruction access fault"); break;
+		case TRAP_CODE_I_ILLEGAL:    printf("Illegal instruction"); break;
+		case TRAP_CODE_BREAKPOINT:   printf("Breakpoint"); break;
+		case TRAP_CODE_L_MISALIGNED: printf("Load address misaligned"); break;
+		case TRAP_CODE_L_ACCESS:     printf("Load access fault"); break;
+		case TRAP_CODE_S_MISALIGNED: printf("Store address misaligned"); break;
+		case TRAP_CODE_S_ACCESS:     printf("Store access fault"); break;
+		case TRAP_CODE_UENV_CALL:    printf("Environment call from U-mode"); break;
+		case TRAP_CODE_MENV_CALL:    printf("Environment call from M-mode"); break;
+		case TRAP_CODE_MSI:          printf("Machine software IRQ"); break;
+		case TRAP_CODE_MTI:          printf("Machine timer IRQ"); break;
+		case TRAP_CODE_MEI:          printf("Machine external IRQ"); break;
+		case TRAP_CODE_FIRQ_0:
+		case TRAP_CODE_FIRQ_1:
+		case TRAP_CODE_FIRQ_2:
+		case TRAP_CODE_FIRQ_3:
+		case TRAP_CODE_FIRQ_4:
+		case TRAP_CODE_FIRQ_5:
+		case TRAP_CODE_FIRQ_6:
+		case TRAP_CODE_FIRQ_7:
+		case TRAP_CODE_FIRQ_8:
+		case TRAP_CODE_FIRQ_9:
+		case TRAP_CODE_FIRQ_10:
+		case TRAP_CODE_FIRQ_11:
+		case TRAP_CODE_FIRQ_12:
+		case TRAP_CODE_FIRQ_13:
+		case TRAP_CODE_FIRQ_14:
+		case TRAP_CODE_FIRQ_15:      printf("Fast IRQ %x", (trap_cause & 0xf)); break;
+		default:                     printf("Unknown trap cause %x", (trap_cause)); break;
+	}
+
+	//   // check if FIRQ
+	//   if ((trap_cause >= TRAP_CODE_FIRQ_0) && (trap_cause <= TRAP_CODE_FIRQ_15)) {
+	//     neorv32_cpu_csr_clr(CSR_MIP, 1 << (CSR_MIP_FIRQ0P + (trap_cause & 0xf))); // clear pending FIRQ
+	//   }
+
+	// instruction address
+	printf(" @ PC=%lx", csr_read(833));
+
+	// trap instruction
+	printf(", MTINST=%lx", csr_read(842));
+
+	// trap value
+	printf(", MTVAL=%lx", csr_read(835));
+
+	printf(" Halting CPU. </NEORV32-RTE>\n");
+	while(1);
+}
 
 #if defined(CONFIG_RISCV_SOC_INTERRUPT_INIT)
 void soc_interrupt_init(void)
@@ -13,5 +79,7 @@ void soc_interrupt_init(void)
 	(void)arch_irq_lock();
 
 	csr_write(mie, 0);
+
+	__asm__ volatile("csrw	mtvec, %[input_j]" : : [input_j] "r" ((uint32_t) &rte_handler));
 }
 #endif
--- soc/riscv/riscv-privileged/neorv32/soc.h
+++ soc/riscv/riscv-privileged/neorv32/soc.h
@@ -64,4 +64,34 @@
 #define NEORV32_SYSINFO_FEATURES_IO_ONEWIRE     BIT(30)
 #define NEORV32_SYSINFO_FEATURES_OCD            BIT(31)
 
+#define TRAP_CODE_I_MISALIGNED 0x00000000U /**< 0.0:  Instruction address misaligned */
+#define TRAP_CODE_I_ACCESS     0x00000001U /**< 0.1:  Instruction (bus) access fault */
+#define TRAP_CODE_I_ILLEGAL    0x00000002U /**< 0.2:  Illegal instruction */
+#define TRAP_CODE_BREAKPOINT   0x00000003U /**< 0.3:  Breakpoint (EBREAK instruction) */
+#define TRAP_CODE_L_MISALIGNED 0x00000004U /**< 0.4:  Load address misaligned */
+#define TRAP_CODE_L_ACCESS     0x00000005U /**< 0.5:  Load (bus) access fault */
+#define TRAP_CODE_S_MISALIGNED 0x00000006U /**< 0.6:  Store address misaligned */
+#define TRAP_CODE_S_ACCESS     0x00000007U /**< 0.7:  Store (bus) access fault */
+#define TRAP_CODE_UENV_CALL    0x00000008U /**< 0.8:  Environment call from user mode (ECALL instruction) */
+#define TRAP_CODE_MENV_CALL    0x0000000bU /**< 0.11: Environment call from machine mode (ECALL instruction) */
+#define TRAP_CODE_MSI          0x80000003U /**< 1.3:  Machine software interrupt */
+#define TRAP_CODE_MTI          0x80000007U /**< 1.7:  Machine timer interrupt */
+#define TRAP_CODE_MEI          0x8000000bU /**< 1.11: Machine external interrupt */
+#define TRAP_CODE_FIRQ_0       0x80000010U /**< 1.16: Fast interrupt channel 0 */
+#define TRAP_CODE_FIRQ_1       0x80000011U /**< 1.17: Fast interrupt channel 1 */
+#define TRAP_CODE_FIRQ_2       0x80000012U /**< 1.18: Fast interrupt channel 2 */
+#define TRAP_CODE_FIRQ_3       0x80000013U /**< 1.19: Fast interrupt channel 3 */
+#define TRAP_CODE_FIRQ_4       0x80000014U /**< 1.20: Fast interrupt channel 4 */
+#define TRAP_CODE_FIRQ_5       0x80000015U /**< 1.21: Fast interrupt channel 5 */
+#define TRAP_CODE_FIRQ_6       0x80000016U /**< 1.22: Fast interrupt channel 6 */
+#define TRAP_CODE_FIRQ_7       0x80000017U /**< 1.23: Fast interrupt channel 7 */
+#define TRAP_CODE_FIRQ_8       0x80000018U /**< 1.24: Fast interrupt channel 8 */
+#define TRAP_CODE_FIRQ_9       0x80000019U /**< 1.25: Fast interrupt channel 9 */
+#define TRAP_CODE_FIRQ_10      0x8000001aU /**< 1.26: Fast interrupt channel 10 */
+#define TRAP_CODE_FIRQ_11      0x8000001bU /**< 1.27: Fast interrupt channel 11 */
+#define TRAP_CODE_FIRQ_12      0x8000001cU /**< 1.28: Fast interrupt channel 12 */
+#define TRAP_CODE_FIRQ_13      0x8000001dU /**< 1.29: Fast interrupt channel 13 */
+#define TRAP_CODE_FIRQ_14      0x8000001eU /**< 1.30: Fast interrupt channel 14 */
+#define TRAP_CODE_FIRQ_15      0x8000001fU  /**< 1.31: Fast interrupt channel 15 */
+
 #endif /* RISCV_NEORV32_SOC_H */
